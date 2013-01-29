a5a0eeeeade365abd3830776b8abbee37ac2db55
r3d32 <cagudo@lion.lmu.edu>
2013-01-29 03:10:37 +0100

    All files created for homework 1

---
+++ homework/canvas-basics/28b.js
@@ -0,0 +1,44 @@
+(function draw(){
+   
+   var ctx = document.getElementById('canvas').getContext('2d');
+
+
+   ctx.beginPath();
+   var canvasW= 480;
+   var canvasH = 300;
+   ctx.fillStyle = 'rgb(0,0,128)'//sky
+   ctx.fillRect(0,0,canvasW,canvasH)
+   
+   //buildings
+   for (var i=0;i<canvasW;){ 
+      ctx.fillStyle = 'rgb(0,0,0)'
+      var x = i; 
+      var rand = Math.floor((Math.random()*4)+0);
+      var arr1 = [60,40,80,100];
+      var width = arr1[rand];
+      var rand2 = Math.floor((Math.random()*25)+0);
+      var arr2 = [30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,250,260,270];
+      var height = arr2[rand2];
+      i += arr1[rand];
+      ctx.fillRect(x,height,width,canvasH);
+
+      //windows
+      ctx.fillStyle = 'rgb(255,255,0)'
+      for (var e = height; e < 300;){
+         for (var t = x ; t < (width + x);){
+            var windowX = t + 4;
+            var windowY = e + 4;
+            var rand3 = Math.floor((Math.random()*5)+0);
+            var arr3 = [8,10,12,0,0];
+            var rand4 = Math.floor((Math.random()*6)+0);
+            var arr4 = [14,16,18,20,24,0];
+            var windowWidth = arr3[rand3];
+            var windowHeight = arr4[rand4];
+            ctx.fillRect(windowX,windowY,windowWidth,windowHeight);
+            t += 20;
+         }
+         e += 30;
+      }
+      
+   }
+}());
