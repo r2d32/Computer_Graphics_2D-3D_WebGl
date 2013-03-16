99fbf5911ef06c751a8c67528be0b4af13ad52e2
r3d32 <cagudo@lion.lmu.edu>
2013-03-16 01:16:58 -0700

    Added basic pyramid to shape library

---
+++ homework/pipeline/shapes.js
@@ -0,0 +1,84 @@
+/*
+ * This module defines/generates vertex arrays for certain predefined shapes.
+ * The "shapes" are returned as indexed vertices, with utility functions for
+ * converting these into "raw" coordinate arrays.
+ */
+var Shapes = {
+    /*
+     * Returns the vertices for a small icosahedron.
+     */
+     pyramid: function () {
+        // These variables are actually "constants" for icosahedron coordinates.
+        
+
+        return {
+            vertices: [
+                [ 0.5, 0.5, 0.5 ],
+                [ 0.5, 0.5, -0.5 ],
+                [ -0.5, 0.5, -0.5 ],
+                [ -0.5, -0.5, 0.5 ]
+            ],
+
+            indices: [
+                [ 0, 1, 2 ],
+                [ 1, 2, 3 ],
+                [ 2, 0, 3 ],
+                [ 1, 2, 3 ]
+            ]
+        };
+    },
+
+
+    /*
+     * Utility function for turning indexed vertices into a "raw" coordinate array
+     * arranged as triangles.
+     */
+    toRawTriangleArray: function (indexedVertices) {
+        var result = [],
+            i,
+            j,
+            maxi,
+            maxj;
+
+        for (i = 0, maxi = indexedVertices.indices.length; i < maxi; i += 1) {
+            for (j = 0, maxj = indexedVertices.indices[i].length; j < maxj; j += 1) {
+                result = result.concat(
+                    indexedVertices.vertices[
+                        indexedVertices.indices[i][j]
+                    ]
+                );
+            }
+        }
+
+        return result;
+    },
+
+    /*
+     * Utility function for turning indexed vertices into a "raw" coordinate array
+     * arranged as line segments.
+     */
+    toRawLineArray: function (indexedVertices) {
+        var result = [],
+            i,
+            j,
+            maxi,
+            maxj;
+
+        for (i = 0, maxi = indexedVertices.indices.length; i < maxi; i += 1) {
+            for (j = 0, maxj = indexedVertices.indices[i].length; j < maxj; j += 1) {
+                result = result.concat(
+                    indexedVertices.vertices[
+                        indexedVertices.indices[i][j]
+                    ],
+
+                    indexedVertices.vertices[
+                        indexedVertices.indices[i][(j + 1) % maxj]
+                    ]
+                );
+            }
+        }
+
+        return result;
+    }
+
+};
