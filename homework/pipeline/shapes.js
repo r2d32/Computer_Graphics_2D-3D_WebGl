88914aab034d1dd2ef26238c3bd79573f74d4885
dondi <dondi@lmu.edu>
2013-04-06 23:26:43 -0700

    Cleaned up index (face) arrays for pencilTip and pencilBody polygon meshes.

--- homework/pipeline/shapes.js
+++ homework/pipeline/shapes.js
@@ -4,6 +4,12 @@
  * converting these into "raw" coordinate arrays.
  */
 var Shapes = {
+    // JD: This is an interesting idea, but it will only work under
+    //     a very specific assumption---namely, that the given array
+    //     of vertices is arranged in such a way that they all form
+    //     triangles according to the order listed.
+    //
+    //     This is not a very general assumption.
     getIndices: function (a) {
         var b =[];
         for (var e = 0; e < a.length; e++){
@@ -34,6 +40,12 @@ var Shapes = {
         return {
             vertices: v,
 
+            // JD: See above: you don't gain much by replacing this
+            //     with your getIndices function, especially for a
+            //     mesh that is as simple as this.  You capture a
+            //     routine as a function when that computation will
+            //     see a lot of use in a lot of situations.  That is
+            //     simply not the case with general polygon meshes.
             indices: Shapes.getIndices(v)
         };
     },
@@ -116,24 +128,35 @@ var Shapes = {
             Yheight = -0.95;
         //This creates the first 6 coordinates of the vertices array that represent an hexagon
         for (var i = 0; i <= numberOfSides;i += 1) {
+            // JD: Why is this vertex an array of arrays?
             v[i] =  [[Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides) ],[ Ycenter ],
                     [ Zcenter + size * Math.sin(i * 2 * Math.PI / numberOfSides) ]];
         }
         //This adds the tip coordinate at the end of the array 
         v[numberOfSides] = [ Xcenter,Yheight,Zcenter ]; 
-        
-        for(var e = 0; e < numberOfSides; e++){
-            if(e == numberOfSides - 1 ){
-                ind.push([e,0,1],
-                         [e,0,numberOfSides]);
-            } else if(e == numberOfSides-2){
-                    ind.push([e,e+1,0],
-                             [e,e+1,numberOfSides]);
-            } else{
-                    ind.push([e,e+1,e+2],
-                             [e,e+1,numberOfSides]);
-            }
-        
+
+        // JD: If you think about it, your polygon mesh should have the same
+        //     number of triangles as you have sides (your pencil tip is
+        //     essentially a hollow cone, as far as I can tell).
+        //     This routine produces *twice* that many triangles!
+//        for(var e = 0; e < numberOfSides; e++){
+//            if(e == numberOfSides - 1 ){
+//                ind.push([e,0,1],
+//                         [e,0,numberOfSides]);
+//            } else if(e == numberOfSides-2){
+//                    ind.push([e,e+1,0],
+//                             [e,e+1,numberOfSides]);
+//            } else{
+//                    ind.push([e,e+1,e+2],
+//                             [e,e+1,numberOfSides]);
+//            }
+//        
+//        }
+        // JD: Compare the array you produce above to this one:
+        for (i = 0; i < numberOfSides; i += 1) {
+            // JD: One triangle per side.  Note the mod at the end
+            //     because the last triangle wraps around to vertex 0.
+            ind.push([numberOfSides, i, (i + 1) % numberOfSides]);
         }
 
         return {
@@ -157,46 +180,74 @@ var Shapes = {
             Zcenter = 0,
             Yheight = -0.2;
 
-        for (var i = 0; i <= numberOfSides;i += 1) {
+        // JD: You had <= below when you really should have done <
+        //     (otherwise v[i + numberOfSides] gets overwritten.
+        for (var i = 0; i < numberOfSides;i += 1) {
+            // JD: Again, I am not clear here.  You are assigning different
+            //     elements of the v array with different data types.  The
+            //     first line assigns an array of arrays.  The next line
+            //     assigns a plain array.  What exactly is your intent here?
             v[i] =  [[Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides) ],[ Ycenter ],
                     [ Zcenter + size * Math.sin(i * 2 * Math.PI / numberOfSides) ]];
 
             v[ i + numberOfSides ] = [ v[i][0], Yheight, v[i][2] ];
         }
-            
-            v[6] = [ v[0][0], Yheight, v[0][2] ];
+            // JD: I don't get this.  Why is v[6] so special?
+            //     (commenting this out)
+//            v[6] = [ v[0][0], Yheight, v[0][2] ];
 
             
+        // JD: Again, think this through.  You are forming a hollow "cylinder"
+        //     consisting of numberOfSides rectangles.  There are two triangles
+        //     per rectangle.  Thus, your index array should have 2 * numberOfSides
+        //     entries.  The code below has many more sides than that!
+        //
+        //     Have you plotted these vertices on paper and formed triangles from
+        //     them?  That is pretty much how you would form the indices array.
+//        for (var e = 0; e < v.length; e++){
+//
+//            if ( e < numberOfSides){
+//                if(e == numberOfSides-2){
+//                    ind.push([e,e+1,0],
+//                             [e, e+numberOfSides, e+numberOfSides+1],
+//                             [e, e+1, e+numberOfSides+1]);
+//                } else if (e == numberOfSides - 1 ){
+//                    ind.push([e,0,1],
+//                             [e, e+numberOfSides, numberOfSides],
+//                             [e, 0, numberOfSides]);
+//                } else{
+//                    ind.push([e,e+1,e+2],
+//                             [e, e+numberOfSides, e+numberOfSides+1],
+//                             [e, e+1, e+numberOfSides+1]);
+//                }
+//
+//            }else{
+//                if(e == v.length-2){
+//                    ind.push([e,e+1,numberOfSides]);
+//                } else if (e == v.length - 1 ){
+//                    ind.push([e,numberOfSides,numberOfSides+1]);
+//                } else{
+//                    ind.push([e,e+1,e+2]);
+//                }
+//            }
+//            
+//        }
+        
+        // JD: One more freebie for you and that's it.  Here is a version
+        //     that builds your index array that produces the precise number
+        //     of triangles required for the hollow cylinder.  Note how I
+        //     even followed the pattern of your vertex generation code
+        //     (minus the funky array-of-arrays approach which I still
+        //     don't understand).
+        for (i = 0; i < numberOfSides; i += 1) {
+            // JD: Note this calculation which accounts for the wraparound.
+            var nextVertexCenter = (i + 1) % numberOfSides,
+                nextVertexHeight = nextVertexCenter + numberOfSides;
 
-        for (var e = 0; e < v.length; e++){
-
-            if ( e < numberOfSides){
-                if(e == numberOfSides-2){
-                    ind.push([e,e+1,0],
-                             [e, e+numberOfSides, e+numberOfSides+1],
-                             [e, e+1, e+numberOfSides+1]);
-                } else if (e == numberOfSides - 1 ){
-                    ind.push([e,0,1],
-                             [e, e+numberOfSides, numberOfSides],
-                             [e, 0, numberOfSides]);
-                } else{
-                    ind.push([e,e+1,e+2],
-                             [e, e+numberOfSides, e+numberOfSides+1],
-                             [e, e+1, e+numberOfSides+1]);
-                }
-
-            }else{
-                if(e == v.length-2){
-                    ind.push([e,e+1,numberOfSides]);
-                } else if (e == v.length - 1 ){
-                    ind.push([e,numberOfSides,numberOfSides+1]);
-                } else{
-                    ind.push([e,e+1,e+2]);
-                }
-            }
-            
+            ind.push([i, nextVertexCenter, i + numberOfSides]);
+            ind.push([nextVertexCenter, nextVertexHeight, i + numberOfSides]);
         }
-            
+
         return {
 
             vertices: v,
