cb123af6dac06b73882b2877413d77e5758d98da
r3d32 <cagudo@lion.lmu.edu>
2013-05-10 20:11:33 -0700

    linked scene files to correct matrix4x4.js, cleaned rep files and added camera matrix

--- homework/pipeline/Matrix4x4/matrix4x4.js
+++ homework/pipeline/Matrix4x4/matrix4x4.js
@@ -26,26 +26,26 @@ var Matrix4x4 = (function () {
 
     },
     
-        // A private method for checking dimensions,
-        // throwing an exception when different.
+    // A private method for checking dimensions,
+    // throwing an exception when different.
     checkDimensions = function (m1) {
+
         if (m1.dimensions() !== 16) {
-            throw "Matri should be 4x4";
+            throw "Matrix should be 4x4";
         }
     };
     
-    //HW change this methods into 
-    // ortho = funcion()
-    ortho = function(l, r, b, t, n, f){
+    matrix4x4.prototype.ortho = function(l, r, b, t, n, f){
         var width = r - l,
             height = t - b,
             depth = f - n;
-
-        return new Matrix4x4 (
+        var or = new Matrix4x4 (
             2.0/width,   0.0,          0.0,        -(r + l)/width,
-            0.0,         2.0/height,   0.0,        -(t+b)/height,
-            0.0,         0.0,         -2.0/depth,  -(f+n)/depth,
-            0.0,         0.0,          0.0,         1.0);
+            0.0,         2.0/height,   0.0,        -(t+b)/height ,
+            0.0,         0.0,         -2.0/depth,  -(f+n)/depth  ,
+            0.0,         0.0,          0.0,         1.0          );
+
+        return or;
     };
 
     matrix4x4.prototype.multiply = function (b) {
@@ -80,13 +80,8 @@ var Matrix4x4 = (function () {
         return this.elements.length;
     };
 
-    // JD: I'm not clear on what this is supposed to do, especially
-    //     in light of what your unit test produces.  A little
-    //     header comment will help.
+
     // Preparation of matrix for consumption by WebGL and GLSL
-    //
-    // JD 0402: OK, this is better.  Last time you were returning
-    //     a string.
     matrix4x4.prototype.conversion = function (){
         var changed = [],
             i,
@@ -105,55 +100,24 @@ var Matrix4x4 = (function () {
         return this.elements;
     };
 
-    // JD: Careful here---the way you implement these matrices
-    //     implies to me that you might not fully understand how
-    //     to use them.  Remember that the transformations
-    //     themselves are formed in well-defined ways: they
-    //     are not based on some pre-existing matrix.  This is
-    //     why, in class, I implement those in a manner that is
-    //     similar to a static method in Java.
-    //
-    //     These standalone matrices are then *multiplied* to
-    //     other matrices or to a vertex in order to "execute"
-    //     the transformation.  This is the other alternative
-    //     for these transforms: form the standalone matrix then
-    //     multiply it with a pre-existing one.
-    //
-    //     The issue with your implementations is that they do
-    //     neither.  Instead, they do some direct assignments to
-    //     the elements of a pre-existing matrix.  These are thus
-    //     not the "standalone" version, nor are they the correct
-    //     way to perform multiplication.  I strongly suggest that
-    //     you pay close attention to upcoming classes in order to
-    //     ensure that you are understanding this correctly.
-    //
-    //     Note, for example, the difference between ortho up top,
-    //     and the implementations you have below.  ortho does the
-    //     "standalone matrix" approach correctly.
-
-    //  Scaling matrix method
+    // Scaling matrix method
     matrix4x4.prototype.scale = function (sx,sy,sz){
-
         var scalator = new Matrix4x4(sx, 0, 0, 0,
                                       0,sy, 0, 0,
                                       0, 0, sz,0,
                                       0, 0, 0, 1);
         
-                    
-        
-
         return scalator.multiply(this);
     };
 
     // Traslating matrix method
     matrix4x4.prototype.translate = function (dx,dy,dz){
-        var translated = new Matrix4x4();
-        translated.elements = this.elements;    
-        translated.elements[3] = dx;
-        translated.elements[7] = dy;
-        translated.elements[11] = dz;
+        var translated = new Matrix4x4(1, 0, 0, dx,
+                                       0, 1, 0, dy,
+                                       0, 0, 1, dz,
+                                       0, 0, 0, 1);
 
-        return translated;
+        return translated.multiply(this);
     };
 
 
@@ -187,28 +151,44 @@ var Matrix4x4 = (function () {
         ys = y * s;
         zs = z * s;
 
-        return new Matrix4x4(
-            (x2 * oneMinusC) + c,   (xy * oneMinusC) - zs,  (xz * oneMinusC) + ys, 0,
-            (xy * oneMinusC) + zs,   (y2 * oneMinusC) + c,  (yz * oneMinusC) - xs, 0,
-            (xz * oneMinusC) - ys,  (yz * oneMinusC) + xs,   (z2 * oneMinusC) + c, 0,
+        var rotation = new Matrix4x4(
+            (x2 * oneMinusC) + c ,  (xy * oneMinusC) - zs,  (xz * oneMinusC) + ys, 0,
+            (xy * oneMinusC) + zs,  (y2 * oneMinusC) + c ,  (yz * oneMinusC) - xs, 0,
+            (xz * oneMinusC) - ys,  (yz * oneMinusC) + xs,  (z2 * oneMinusC) + c , 0,
                                 0,                      0,                      0, 1);
+
+        return rotation.multiply(this);
     };
     
     // Matrix frustum perspective
     matrix4x4.prototype.frustum = function(l,r,b,t,n,f){
-        var fm = new Matrix4x4();
-        fm.elements = this.elements;
-        fm.elements[0]  = 2 * n / (r - l);
-        fm.elements[2]  = (r + l) / (r - l);
-        fm.elements[5]  = 2 * n / (t - b);
-        fm.elements[6]  = (t + b) / (t - b);
-        fm.elements[10] = -(f + n) / (f - n);
-        fm.elements[11] = -(2 * f * n) / (f - n);
-        fm.elements[14] = -1;
-        fm.elements[15] = 0;
-
+        var fm = new Matrix4x4(
+            2 * n / (r - l),               0,  (r + l) / (r - l),                      0, 
+                          0, 2 * n / (t - b),  (t + b) / (t - b),                      0,
+                          0,               0, -(f + n) / (f - n), -(2 * f * n) / (f - n),
+                          0,               0,                 -1,                      0);
+    
         return fm;
     }
+
+    //Returns a camera matrix.   
+    matrix4x4.camera = function(px, py, pz, qx, qy, qz, ux, uy, uz){
+        var up = new Vector(ux, uy, uz),
+            pvector = new Vector(px, py, pz),
+            qvector = new Vector(qx, qy, qz),
+            ze = (pvector.subtract(qvector)).unit(),    
+            ye = up.subtract(up.projection(ze)).unit(),
+            xe = ye.cross(ze);
+            
+        var cam = new Matrix4x4(
+            xe.x(), xe.y(), xe.z(), -(pvect.dot(xe)),
+            ye.x(), ye.y(), ye.z(), -(pvect.dot(ye)),
+            ze.x(), ze.y(), ze.z(), -(pvect.dot(ze)),
+                 0,      0,      0,                1);
+
+            return cam;
+            
+    };
 
     return matrix4x4;
 })();
