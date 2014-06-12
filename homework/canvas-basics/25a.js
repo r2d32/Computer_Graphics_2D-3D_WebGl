0916bb800b5e6d9c2e31c39c23ea5b44bef07390
r3d32 <cagudo@lion.lmu.edu>
2014-06-11 19:22:08 -0700

    with '#' will be ignored, and an empty message aborts the commit.

--- homework/canvas-basics/25a.js
+++
@@ -1,8 +0,0 @@
-/*
- * Exercise 25a
- */
-(function draw(){
-   var ctx = document.getElementById('canvas').getContext('2d');
-   ctx.fillStyle = "rgb(0,0,200)";
-   ctx.fillRect(60,60,60,60);
-}());
